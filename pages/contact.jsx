d9f3b4279606cf919cfce50a4eec4e5e4018da72
Ploy <117616170+yaminP@users.noreply.github.com>
2023-01-26 10:22:49 +0700

    img

---
+++ pages/contact.jsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import React, { useRef } from "react";
+
+export default function ContactPage() {
+
+  return (
+    <>
+     <Head>
+        <title>ช่องทางติดต่อ</title>
+      </Head>
+      <div className="container p-16 mx-auto">
+        <h2 className="text-3xl text-center">ช่องทางติดต่อ</h2>
+        <div className="gird grid-col-4">
+            <div className="">
+
+            </div>
+            <div className="col-span-2">
+                
+            </div>
+        </div>
+      </div>
+
+    </>
+  );
+}

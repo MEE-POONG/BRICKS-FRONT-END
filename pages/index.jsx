8b5ccb6f83be61602a829721e3e7199b14c4c15d
Ploy <117616170+yaminP@users.noreply.github.com>
2023-01-27 15:03:18 +0700

    footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import React, { useRef } from "react";
+import Layout from "../Layout/Layout";
+import Showproducts from "./home/showproductype";
 import HomeTop from "./home/top";
 
 export default function Home() {
@@ -10,6 +12,14 @@ export default function Home() {
         <title>ช่างพองอิฐประสาน</title>
           <link rel="icon" href="/logo.png" />
       </Head>
+      <div>
+        <HomeTop/>
+      </div>
+      <div>
+      <Showproducts/>
+      </div>
+
     </>
   );
 }
+Home.layout = Layout;

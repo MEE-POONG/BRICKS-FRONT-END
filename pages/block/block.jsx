a5de77bf068098fbd5ae1c5f746f95ebfede850a
Ploy <117616170+yaminP@users.noreply.github.com>
2023-01-28 14:07:11 +0700

    block

---
+++ pages/block/block.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+export default function ShowBlocks() {
+  return (
+    <>
+      <div className=" container mx-auto text-center font-fontTh2 mt-10">
+        <h3 className="text-[24px] md:text-[30px] text-primary mb-5">
+          {" "}
+          บทความ
+          <span className="text-gray-400"> | Blocks </span>
+        </h3>
+        <div className="mx-2">
+          <Swiper
+            spaceBetween={30}
+            slidesPerView={3}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            breakpoints={{
+              // when window width is >= 640px
+              300: {
+                width: 300,
+                slidesPerView: 1,
+              },
+            }}
+          >
+            <SwiperSlide>
+              <div className="max-w-sm rounded overflow-hidden shadow-xl border">
+                <img
+                  className="w-full"
+                  src="/images/bw3.jpg"
+                  alt="Sunset in the mountains"
+                />
+                  <div className="font-bold text-xl px-6 py-4 mb-2 truncate ">
+                  หัวข้อบล็อกหัวข้อบล็อกหัวข้อบล็อกหัวข้อหัวข้อหัวข้อหัวข้อหัวข้อหัวข้อหัวข้อหัวข้อหัวข้อหัวข้อหัวข้อ
+                  </div>
+                <div className="px-6 pb-2 ">
+                  <a
+                    href="/block/block_detail"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-primary hover:underline  "
+                  >
+                    Read more
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4 ml-2 -mr-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ pages/block/block_detail.jsx
@@ -0,0 +1,88 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useRef } from "react";
+
+export default function BlockDetail() {
+  return (
+    <>
+      <div className="container pt-16 mx-auto font-fontTh2 p-4">
+        <div className="px-6 pb-2 mb-10 md:mb-24 ">
+          <a
+            href="../"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-primary hover:underline  "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-arrow-return-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
+              />
+            </svg>
+            Back
+          </a>
+        </div>
+        {/* <section className="text-gray-700 body-font overflow-hidden bg-white rounded-lg shadow-xl p-3 lg:p-10"> */}
+        <img
+          src="/images/bw3.jpg"
+          className="w-auto h-auto lg:h-96 rounded-md mx-auto"
+          alt=""
+        />
+        <h2 className="mt-24 text-center text-xl md:text-3xl font-bold">
+          หัวข้อบทความที่ต้องการนำเสนอ
+        </h2>
+
+        <div className="m-4 md:m-12 indent-8 whitespace-normal text-justify">
+          <p>
+            ช่วงนี้กระแสการตกแต่งบ้าน ร้านอาหาร คาเฟ่ หรือสถานที่ท่องเที่ยวต่าง
+            ๆ ด้วยอิฐแดงช่องลมกำลังมาแรง เพราะนอกจากจะช่วยเพิ่มความสวยงาม
+            เป็นเอกลักษณ์แบบไม่ซ้ำใครแล้ว
+            อิฐแดงช่องลมยังมีประโยชน์ในเรื่องของการเป็นช่องรับลม
+            และรับแสงจากธรรมชาติ หลายคนจึงนิยมนำไปก่อเป็นฟาซาด กำแพง
+            หรือผนังกั้นทั้งภายใน และภายนอก แต่สงสัยกันไหมว่า
+            อิฐแดงช่องลมนั้นมีวิธีการก่ออย่างไรให้แข็งแรง ปลอดภัย
+            เนื่องจากเป็นการก่อแบบโชว์แนว ไม่มีการฉาบปกปิด
+            จึงเป็นเรื่องยากที่จะเห็นการใส่เสาเอ็น และคานทับหลัง
+            ที่ทำหน้าที่เป็นตัวยยึดผนังให้แข็งแรง ถ้าอย่างนั้น ต้องก่ออย่างไร?
+            ในบทคามนี้มีคำตอบครับ กับ How To ก่ออิฐแดงช่องลมอย่างไร ให้แข็งแรง
+            สวยงาม
+          </p>
+        </div>
+        <div className="flex mx-5 md:mx-10 flex-wrap-reverse mt-10">
+          <div className="flex-auto w-96 md:mr-10 my-10 indent-8 ">
+            <h4 className="text-lg text-primary font-semibold mb-5">
+              หัวข้อย่อย
+            </h4>
+            <p>
+              ช่วงนี้กระแสการตกแต่งบ้าน ร้านอาหาร คาเฟ่
+              หรือสถานที่ท่องเที่ยวต่าง ๆ ด้วยอิฐแดงช่องลมกำลังมาแรง
+              เพราะนอกจากจะช่วยเพิ่มความสวยงาม เป็นเอกลักษณ์แบบไม่ซ้ำใครแล้ว
+              อิฐแดงช่องลมยังมีประโยชน์ในเรื่องของการเป็นช่องรับลม
+            </p>
+          </div>
+          <div className="flex-auto w-32">
+            <img
+              src="/images/bw3.jpg"
+              className="w-full h-auto rounded-md "
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="mx-10 mt-24 text-end text-primary hover:underline">
+            <i>
+                แหล่งที่มา :<a href="#">ร้านช่างพอง</a>
+            </i>
+        </div>
+       
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
+}

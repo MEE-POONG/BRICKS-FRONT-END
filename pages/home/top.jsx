d1a0bfe5836ccb3c3f29c6c8a6db345ce1c151c3
Ploy <117616170+yaminP@users.noreply.github.com>
2023-01-31 10:00:19 +0700

    hometop

--- pages/home/top.jsx
+++ pages/home/top.jsx
@@ -21,27 +21,27 @@ export default function HomeTop() {
         <div className=" py-10 grid lg:grid grid-cols-1 lg:grid-cols-3 gap-4 text-center mx-5 lg:mx-40">
           <div className="max-w-sm bg-white border-2 border-primary rounded-lg shadow-lg  ">
             <div className="p-5">
-              <h5 className="mb-2 text-2xl  ">ติดต่อ</h5>
-                <button className=" bg-primary rounded-3xl p-2">
-                  <a href="tel:094 397 0414" className=" text-white">094 397 0414</a>
-                </button>
+            <h5 className="mb-2 text-2xl  tracking-tight  ">ต่อคนในองค์กร</h5>
+              <p className="mb-3 font-normal ">
+                ส่งเสริมให้ทีมงานเป็นผู้เรียนรู้ ตลอดชีวิต
+                มีคุณภาพชีวิตและความมั่นคง
+              </p>
             </div>
           </div>
           <div className="max-w-sm bg-white border-2 border-primary rounded-lg shadow-lg">
             <div className="p-5">
-              <h5 className="mb-2 text-2xl  tracking-tight  ">ต่อลูกค้า</h5>
-              <p className="mb-3 font-normal ">
-                ซื่อสัตย์ เป็นธรรม ส่งเสริมความก้าวหน้า
+              <h5 className="mb-2 text-2xl  tracking-tight  ">ช่องทางติดตาม</h5>
+              <p className="mb-3 font-normal text-primary hover:underline">
+                <a href="https://www.facebook.com/DreamBlock9">ร้านช่างพอง อิฐประสาน จัดสวน สร้างบ้าน ราคาโรงงาน</a>
               </p>
             </div>
           </div>
           <div className="max-w-sm bg-white border-2 border-primary rounded-lg shadow-lg  ">
             <div className="p-5">
-              <h5 className="mb-2 text-2xl  tracking-tight  ">ต่อคนในองค์กร</h5>
-              <p className="mb-3 font-normal ">
-                ส่งเสริมให้ทีมงานเป็นผู้เรียนรู้ ตลอดชีวิต
-                มีคุณภาพชีวิตและความมั่นคง
-              </p>
+            <h5 className="mb-2 text-2xl  ">ติดต่อ</h5>
+                <button className=" bg-primary rounded-3xl p-2">
+                  <a href="tel:094 397 0414" className=" text-white">094 397 0414</a>
+                </button>
             </div>
           </div>
         </div>

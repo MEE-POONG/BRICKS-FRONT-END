0a3a21701f6f7a2def96c7b9b97f656e15a703b5
Pattawut Biakrathok <62004847+bens0746@users.noreply.github.com>
2023-02-09 10:44:13 +0700

    update facebook iframe

--- components/Portfolio/FacebookPost.jsx
+++ components/Portfolio/FacebookPost.jsx
@@ -1,16 +1,15 @@
 import useAxios from "axios-hooks";
 import React from "react";
-import { FacebookProvider, EmbeddedPost } from "react-facebook";
 import Loading from "../Loading/Loading";
 
-//รอเปลี่ยน เป็นของ บ.
-const AppID = "720522252954036";
-const AccessToken =
-  "EAAKPT6jwqbQBAGF96HHNQZAxe0nSdTCmFVvZBe6HZA3ZCl5fItdyLtTRvQlwyQPiQu2EiMpgtL61ZCL4pDhqGOKkpq1IOSsZBJhZBQZCqTU09ZB6Q0o10zmhtdpGQngqAS33tbwZCqWl4tA0flZAXypeOxkIz3rjAyojSWtV5yie79sDtIAtvlHZAhS7";
-const PageId = "103534424506037";
-const Limit = "12";
-
 export default function FacebookPost() {
+  //รอเปลี่ยน เป็นของ บ.
+  const AppID = "720522252954036";
+  const AccessToken =
+    "EAAKPT6jwqbQBAGF96HHNQZAxe0nSdTCmFVvZBe6HZA3ZCl5fItdyLtTRvQlwyQPiQu2EiMpgtL61ZCL4pDhqGOKkpq1IOSsZBJhZBQZCqTU09ZB6Q0o10zmhtdpGQngqAS33tbwZCqWl4tA0flZAXypeOxkIz3rjAyojSWtV5yie79sDtIAtvlHZAhS7";
+  const PageId = "103534424506037";
+  const Limit = "12";
+
   const [
     {
       data: facebookFeeds,
@@ -28,7 +27,7 @@ export default function FacebookPost() {
     .map((postId) => {
       return postId.id.split("_")[1];
     });
-  // return dataPosts.id.split("_")[1];
+
   console.log("facebookpostID", posts);
   if (facebookFeedsError) {
     <div>error</div>;
@@ -36,21 +35,23 @@ export default function FacebookPost() {
 
   return (
     <>
-      <meta property="fb:app_id" content={`&#123;${AppID}&#125;`} />
       <div className="min-h-screen p-10 bg-gray-100">
         {facebookFeedsLoading ? (
           <Loading />
         ) : (
           <>
-            <div className="grid grid-cols-1 gap-x-6 gap-y-12 md:grid-cols-2 lg:grid-cols-3">
+            <div className="grid grid-cols-1 gap-x-6 md:grid-cols-2 lg:grid-cols-3">
               {posts?.map((id, index) => (
-                <div key={index}>
-                  <FacebookProvider appId={AppID}>
-                    <EmbeddedPost
-                      href={`https://www.facebook.com/sn4ppy/posts/${id}`}
-                      lazy={true}
-                    />
-                  </FacebookProvider>
+                <div key={index} className="w-auto">
+                  <iframe
+                    src={`https://www.facebook.com/plugins/post.php?href=https://www.facebook.com/sn4ppy/posts/${id}&show_text=true&appId=${AppID}&width=600`}
+                    data-href={`https://www.facebook.com/plugins/post.php?href=https://www.facebook.com/sn4ppy/posts/${id}`}
+                    frameborder="0"
+                    data-lazy={true}
+                    allowfullscreen="true"
+                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                    className="w-full min-h-screen overflow-hidden"
+                  ></iframe>
                 </div>
               ))}
             </div>

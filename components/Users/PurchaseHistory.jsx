c7f0e34442b302bdc6eb1678071b3e55a0a6187b
Primpunyaporn <117614438+Primpunyaporn@users.noreply.github.com>
2023-02-15 16:34:10 +0700

    pppp

--- components/Users/PurchaseHistory.jsx
+++ components/Users/PurchaseHistory.jsx
@@ -1,80 +1,177 @@
+import { useSession, signOut } from "next-auth/react";
+import useAxios from "axios-hooks";
+import _ from "lodash";
+import { useRouter } from "next/router";
 import React from "react";
+import { FaBars, FaRegUser, FaShoppingBasket, FaSistrix } from "react-icons/fa";
 import SectionComponent from "../headtop";
 
-export default function PurchaseHistory () {
-    return (
-        <>
+export default function PurchaseHistory() {
 
- <SectionComponent title="ประวัติการซื้อของฉัน" detail="" >
-   <div className="container mx-auto px-4 sm:px-8 ">
-    <div className="py-2">
-        <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto text-2xl">
-            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                <table className="min-w-full leading-normal px-5 py-4 border-b-2 border-gray-200 bg-gray-100 text-center  font-semibold text-gray-600 uppercase tracking-wider ">
-                    <thead>
-                        <tr>
-                            <th
-                                className=" ">
-                                สินค้า
-                            </th>
-                            <th
-                                className="">
-                                จำนวน
-                            </th>
-                            <th
-                                className="">
-                                ราคา/ชิ้น
-                            </th>
-                            <th
-                                className="">
-                                ราคารวม
-                            </th>
-                            <th
-                                className="">
-                                สถานะ
-                            </th>
-                        </tr>
+  return (
+    <>
+      <SectionComponent title="ประวัติการซื้อของฉัน" detail="">
+        <main>
+          <section>
+            <div className="bg-gray-100 sm:grid grid-cols-5 grid-rows-2 px-4 py-6 min-h-full lg:min-h-screen space-y-4 sm:space-y-0 sm:gap-4">
+              <div className="h-40 col-span-4 flex items-center container mx-auto sm:-mx-8  sm:px-8 text-2xl ">
+                <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                  <table className="min-w-full leading-normal px-5 border-b-2 border-gray-200 bg-[#1f2937] text-center  font-semibold text-white uppercase tracking-wider ">
+                    <thead >
+                      <tr className="">
+                        <th className=" ">ลำดับ</th>
+                        <th className="p-2 ">สินค้า</th>
+                        <th className="">จำนวน</th>
+                        <th className="">ราคา/ชิ้น</th>
+                        <th className="">ราคารวม</th>
+                        <th className="">สถานะ</th>
+                        <th className="">สั่งซื้อเมื่อ</th>
+                        <th className="">รายละเอียด</th>
+                      </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl w-2/5">
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0 w-10 h-10 hidden sm:table-cell">
-                                        <img className="w-full h-full rounded-full"
-                                            src="https://images.unsplash.com/photo-1601046668428-94ea13437736?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2167&q=80"
-                                            alt="" />
-                                    </div>
-                                    <div className="ml-3">
-                                        <p className="text-gray-900 whitespace-no-wrap">
-                                            Team 1
-                                        </p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl">
-                                <p className="text-gray-900 whitespace-no-wrap text-center">0</p>
-                            </td>
-                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl">
-                                <p className="text-gray-900 whitespace-no-wrap text-center">
-                                    3
-                                </p>
-                            </td>
-                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl">
-                                <p className="text-gray-900 whitespace-no-wrap text-center">
-                                    3
-                                </p>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-2xl">
-                                    <p class="py-3 px-3 text-sm focus:outline-none leading-none text-red-700 bg-red-100 rounded">Due today at 18:00</p>
-                                </td>
-                        </tr>
+                      <tr>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl  hover:bg-gray-100">
+                          <p className="text-gray-900 whitespace-no-wrap text-center">
+                            1
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl w-2/6 hover:bg-gray-100">
+                        <p className="text-gray-900 whitespace-no-wrap text-center">
+                            แมว เหมียว
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl  hover:bg-gray-100">
+                          <p className="text-gray-900 whitespace-no-wrap text-center">
+                            0333
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl  hover:bg-gray-100">
+                          <p className="text-gray-900 whitespace-no-wrap text-center">
+                            3333
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl  hover:bg-gray-100">
+                          <p className="text-gray-900 whitespace-no-wrap text-center">
+                            3333
+                          </p>
+                        </td>
+                        <td className=" whitespace-no-wrap border-bpx-5 py-5 border-b border-gray-200 bg-white hover:bg-gray-100 ">
+                          <span className="relative inline-block px-10 py-1 font-semibold text-green-900 leading-tight">
+                            <span
+                              aria-hidden
+                              className="absolute inset-0 bg-green-200 opacity-50 rounded-md  "
+                            ></span>
+                            <span className="relative text-xl">
+                                อิอิ
+                                </span>
+                          </span>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-xl  hover:bg-gray-100">
+                          <p className="py-3 px-3 focus:outline-none leading-none text-red-700 bg-red-100 rounded">
+                            15/2/2565 เมื่อ 18:00
+                          </p>
+                        </td>
+                        <td className=" px-5 py-5 border-b border-gray-200 bg-white text-xl  hover:bg-gray-100">
+                          <button className="px-5 py-2 border-blue-500 border text-blue-500 rounded-md transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">
+                            ดูรายละเอียด
+                          </button>
+                        </td>
+                      </tr>
+
+                      <tr>
+                      <td className="px-5 py-5 border-b-3 border-gray-200 bg-white text-2xl  hover:bg-gray-100">
+                          <p className="text-gray-900 whitespace-no-wrap text-center">
+                            1
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl w-2/6 hover:bg-gray-100">
+                        <p className="text-gray-900 whitespace-no-wrap text-center">
+                            แมว เหมียว
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl  hover:bg-gray-100">
+                          <p className="text-gray-900 whitespace-no-wrap text-center">
+                            0333
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl  hover:bg-gray-100">
+                          <p className="text-gray-900 whitespace-no-wrap text-center">
+                            3333
+                          </p>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-2xl  hover:bg-gray-100">
+                          <p className="text-gray-900 whitespace-no-wrap text-center">
+                            3333
+                          </p>
+                        </td>
+                        <td className=" whitespace-no-wrap border-bpx-5 py-5 border-b border-gray-200 bg-white hover:bg-gray-100 ">
+                          <span className="relative inline-block px-10 py-1 font-semibold text-green-900 leading-tight">
+                            <span
+                              aria-hidden
+                              className="absolute inset-0 bg-green-200 opacity-50 rounded-md  "
+                            ></span>
+                            <span className="relative text-xl">
+                                อิอิ
+                                </span>
+                          </span>
+                        </td>
+                        <td className="px-5 py-5 border-b border-gray-200 bg-white text-xl  hover:bg-gray-100">
+                          <p className="py-3 px-3 focus:outline-none leading-none text-red-700 bg-red-100 rounded">
+                            15/2/2565 เมื่อ 18:00
+                          </p>
+                        </td>
+                        <td className=" px-5 py-5 border-b border-gray-200 bg-white text-xl  hover:bg-gray-100">
+                          <button className="px-5 py-2 border-blue-500 border text-blue-500 rounded-md transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">
+                            ดูรายละเอียด
+                          </button>
+                        </td>
+                      </tr>
                     </tbody>
-                </table>
+                  </table>
+                </div>
+              </div>
+              <div className="h-96 col-span-1 ">
+                <div className="bg-white  rounded-md border-primary border text-2xl ">
+                  <h1 className="text-center my-4  bg-white py-2 rounded-md border-b-2  text-gray-600">
+                    Service
+                  </h1>
+                  <div className="bg-white rounded-md list-none  text-center ">
+                    <li className="py-3 border-b-2">
+                      <a href="#" className="list-none  hover:text-indigo-600">
+                        Products
+                      </a>
+                    </li>
+                    <li className="py-3 border-b-2">
+                      <a href="#" className="list-none  hover:text-indigo-600">
+                        Models
+                      </a>
+                    </li>
+                    <li className="py-3 border-b-2">
+                      <a href="#" className="list-none  hover:text-indigo-600">
+                        Pricing
+                      </a>
+                    </li>
+                    <li className="py-3 border-b-2">
+                      <a href="#" className="list-none  hover:text-indigo-600">
+                        Hire
+                      </a>
+                    </li>
+                    <li className="py-3 ">
+                      <a
+                        href="#"
+                        className="list-none border-b-2 hover:text-indigo-600"
+                      >
+                        Business
+                      </a>
+                    </li>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    </div>
-</div>
-</SectionComponent>
-        </>
-    )
+          </section>
+        </main>
+      </SectionComponent>
+    </>
+  );
 }

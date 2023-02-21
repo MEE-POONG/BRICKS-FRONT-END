78b3f2be1c25fb914ad09864b04047f52284d037
mind2421 <thutiya.ch@gmail.com>
2023-02-21 09:34:39 +0700

    update

---
+++ pages/api/address/index.js
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+    const { method } = req
+    switch (method) {
+        case 'GET':
+            try {
+                const data = await prisma.address.findMany();
+                res.status(200).json(data)
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        case 'POST':
+            try {
+                await prisma.address.create({
+                    data: {
+                        address: req.body.address,
+                        district: req.body.district,
+                        subDistrict: req.body.subDistrict,
+                        province: req.body.province,
+                        postalCode: req.body.postalCode,
+                        userId: req.body.userId,
+                    }
+                })
+                res.status(201).json({ success: true })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        default:
+            res.setHeader('Allow', ['GET', 'POST'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+    }
+}

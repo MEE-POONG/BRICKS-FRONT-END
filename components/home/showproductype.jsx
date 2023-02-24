36be6caf7ecf4711bf085711e53adb66129130ce
mind2421 <thutiya.ch@gmail.com>
2023-02-24 19:28:10 +0700

    update

--- components/home/showproductype.jsx
+++ components/home/showproductype.jsx
@@ -4,6 +4,13 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import SectionComponent from "../headtop";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import "swiper/css/autoplay";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function Showproducts() {
   const [{ data: showproData, loading, error }, getProductsHome] = useAxios({
@@ -13,42 +20,54 @@ export default function Showproducts() {
 
   return (
     <>
-
       <SectionComponent title="สินค้าของเรา" detail="Categories">
-        <div className="grid grid-cols-1 lg:grid-cols-5 mt-10">
-          {showproData?.map((productsHome, index) => (
-            <div key={index}>
-              <div className="cardp mt-10">
-                <div className="cardp-img ">
-                  <Image
-                    fill
-                    sizes="100vw"
-                    src={productsHome.products[0]?.image}
-                    alt="productImage"
-                    className=" rounded-xl"
-                  />
+        <div className=" m-6">
+          <Swiper
+            spaceBetween={30}
+            slidesPerView={3}
+            modules={[Autoplay]}
+            autoplay={{ delay: 2000 }}
+            breakpoints={{
+              300: {
+                width: 300,
+                slidesPerView: 1,
+              },
+            }}
+          >
+            {showproData?.map((productsHome, index) => (
+              <SwiperSlide key={index}>
+                <div className="cardp mt-10">
+                  <div className="cardp-img ">
+                    <Image
+                      fill
+                      sizes="100vw"
+                      src={productsHome.products[0]?.image}
+                      alt="productImage"
+                      className=" rounded-xl"
+                    />
+                  </div>
+                  <div className="cardp-info">
+                    <p className="text-title text-2xl lg:text-3xl font-fontTh1">
+                      {productsHome.name}
+                    </p>
+                    <p className="text-body text-xl lg:text-2xl font-fontTh1  text-gray-400 font-bold">
+                      ใช้สำหรับก่องานโครงสร้าง หรือตกแต่งทำเป็นกำแพงในสวน
+                    </p>
+                  </div>
+                  <div className="cardp-footer">
+                    <Link href={`/products/category/${productsHome.type.name}`}>
+                      <button
+                        type="button"
+                        className=" svg-icon text-[18px] lg:text-[18px]  animate-bounce  text-white bg-primary hover:bg-red-700 focus:ring-primary rounded-full  px-16 lg:px-16 py-2 mt-2 text-center mr-2 mb-2  ml-10"
+                      >
+                        ช็อปเลย
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-                <div className="cardp-info">
-                  <p className="text-title text-2xl lg:text-3xl font-fontTh1">
-                    {productsHome.name}
-                  </p>
-                  <p className="text-body text-xl lg:text-2xl font-fontTh1  text-gray-400 font-bold">
-                    ใช้สำหรับก่องานโครงสร้าง หรือตกแต่งทำเป็นกำแพงในสวน
-                  </p>
-                </div>
-                <div className="cardp-footer">
-                  <Link href={`/products/category/${productsHome.type.name}`}>
-                    <button
-                      type="button"
-                      className=" svg-icon text-[18px] lg:text-[18px]  animate-bounce  text-white bg-primary hover:bg-red-700 focus:ring-primary rounded-full  px-16 lg:px-16 py-2 mt-2 text-center mr-2 mb-2  ml-10"
-                    >
-                      ช็อปเลย
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          ))}
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </SectionComponent>
     </>

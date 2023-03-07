d94ec1f26f5b9de6b85f1c07421d79eba1458845
Pattawut Biakrathok <62004847+bens0746@users.noreply.github.com>
2023-03-07 13:59:22 +0700

    update orderBy desc

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -24,6 +24,9 @@ export default async function handler(req, res) {
               },
             },
           },
+          orderBy:{
+            createdAt: 'desc'
+          }
         });
         res.status(200).json(data);
       } catch (error) {

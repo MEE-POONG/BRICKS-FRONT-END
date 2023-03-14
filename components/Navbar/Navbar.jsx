135109a2b2d3a36f140118559b021741215ed3c6
devilzeros <devilzeros@outlook.co.th>
2023-03-14 23:44:01 +0700

    navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -39,9 +39,9 @@ export default function Navbar() {
   return (
     <>
       <header className="sticky top-0 shadow-sm bg-white z-40 text-2xl">
-        <div className="min-w-full relative">
+        <div className="min-w-full my-2 relative">
           <div className="lg:items-center lg:justify-between lg:flex">
-            <div className="flex justify-around space-y-2">
+            <div className="flex justify-around">
               <Link href={"/"} className="lg:mx-10 my-auto">
                 <img
                   src="/logo03.png"
@@ -75,7 +75,7 @@ export default function Navbar() {
                     <FaShoppingBasket />
                   </div>
                   <div className="text-l leading-3">ตะกร้า</div>
-                  <div className="absolute -right-2 -top-2 w-auto h-5 px-2  rounded-full flex items-center justify-center bg-[#a5522a] text-white text-3xl lg:w-5 lg:h-5 lg:-right-2 lg:py-2">
+                  <div className="absolute -right-2 -top-2 w-auto h-5 px-2  rounded-full flex items-center justify-center bg-[#a5522a] text-white text-3xl lg:w-5 lg:h-5 lg:-right-2 lg:py-2  mt-2">
                     {cartLength}
                   </div>
                 </Link>
@@ -92,7 +92,7 @@ export default function Navbar() {
                       </span>
                       <button
                         onClick={handleSignOut}
-                        className="p-2 bg-[#a5522a] rounded-md text-white "
+                        className="p-2 bg-[#a5522a] rounded-md text-white  mt-2"
                       >
                         logout
                       </button>
@@ -106,7 +106,7 @@ export default function Navbar() {
                     <div className="text-lg flex justify-center lg:text-2xl">
                       <FaRegUser />
                     </div>
-                    <div className=" leading-3 text-2xl">เข้าสู่ระบบ</div>
+                    <div className=" leading-3 text-2xl mt-2">เข้าสู่ระบบ</div>
                   </Link>
                 )}
               </div>
@@ -114,22 +114,22 @@ export default function Navbar() {
           </div>
         </div>
 
-        <nav className="mt-2 bg-gray-800">
+        <nav className="bg-gray-800">
           {/* Full Display */}
           <div className=" container flex font-fontTh2">
-            <div className="px-8 py-4 ml-10 lg:ml-24 bg-[#a5522a] flex items-center cursor-pointer relative group/type">
+            <div className="px-8 py-4 ml-10 lg:ml-24  flex items-center cursor-pointer relative group/type hover/type:bg-[#a5522a]">
               <span className="text-white">
                 <FaBars />
               </span>
               <span className="ml-2 font-bold text-white">ประเภทสินค้า</span>
-              <div className="absolute w-full z-40 left-0 top-full  bg-[#a5522a] rounded-b-lg shadow-md py-3 divide-y divide-white divide-solid  opacity-0 group-hover/type:opacity-100 transition duration-300 invisible group-hover/type:visible">
+              <div className="absolute w-full z-40 left-0 top-full rounded-b-lg shadow-md py-3 divide-y divide-white divide-solid bg-gray-800 opacity-0 group-hover/type:opacity-100 transition invisible group-hover/type:visible">
                 {arrSortTypeName?.map((type, index) => (
                   <div
                     key={index}
                     onClick={(e) =>
                       handleClick(e, "/products/category", type?.name)
                     }
-                    className="font-bold text-[24px] flex items-center px-6 py-3 hover:bg-red-500 transition relative group/subType"
+                    className="font-bold text-[24px] flex items-center px-6 py-3 hover:bg-[#a5522a] transition relative group/subType"
                   >
                     <span className="ml-6 text-white text-[24px]">
                       {type.name}
@@ -138,41 +138,41 @@ export default function Navbar() {
                 ))}
               </div>
             </div>
-            <div className="hidden items-center justify-between flex-grow pl-12 lg:flex font-bold">
-              <div className="flex items-center space-x-6 capitalize">
+            <div className="hidden items-center justify-between flex-grow lg:flex font-bold">
+              <div className="flex items-center capitalize  h-full">
                 <Link
                   href="/products"
-                  className="text-gray-200 hover:text-white transition"
+                  className="text-gray-200 flex items-center px-4 hover:text-white transition h-full hover:bg-[#a5522a]"
                 >
                   สินค้า
                 </Link>
                 <Link
                   href="/portfolio"
-                  className="text-gray-200 hover:text-white transition"
+                  className="text-gray-200 flex items-center px-4 hover:text-white transition h-full hover:bg-[#a5522a]"
                 >
                   ผลงานของเรา
                 </Link>
                 <Link
                   href="/about"
-                  className="text-gray-200 hover:text-white transition"
+                  className="text-gray-200 flex items-center px-4 hover:text-white transition h-full hover:bg-[#a5522a]"
                 >
                   เกี่ยวกับเรา
                 </Link>
                 <Link
                   href="/contact"
-                  className="text-gray-200 hover:text-white transition"
+                  className="text-gray-200 flex items-center px-4 hover:text-white transition h-full hover:bg-[#a5522a]"
                 >
                   ติดต่อเรา
                 </Link>
                 <Link
                   href="/howtoplaceOder"
-                  className="text-gray-200 hover:text-white transition"
+                  className="text-gray-200 flex items-center px-4 hover:text-white transition h-full hover:bg-[#a5522a]"
                 >
                   วิธีการสั่งซื้อ
                 </Link>
                 {/* <Link
                 href="/article"
-                className="text-gray-200 hover:text-white transition"
+                className="text-gray-200 flex items-center hover:text-white transition"
               >
                 แหล่งความรู้
               </Link> */}

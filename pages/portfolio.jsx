5f0ff097a9e202fff8e608dad03a025ac7fd356e
Ploy <117616170+yaminP@users.noreply.github.com>
2023-05-18 17:38:08 +0700

    portfolio

---
+++ pages/portfolio.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FacebookPost from "../components/Portfolio/FacebookPost";
+
+export default function Portfolio() {
+  return (
+    <>
+      <FacebookPost />
+    </>
+  );
+}
